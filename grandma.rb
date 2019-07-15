af91433cd2ffa76198169980e6e9d582dc9516b7
Audrea Cook <audreacwork@gmail.com>
2019-07-15 16:10:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
 def speak_to_grandma(message)
   if message == 'I LOVE YOU GRANDMA!'
     "I LOVE YOU TOO PUMPKIN!"
-    elsif message !== message.upcase
+    elsif message.upcase
+      "NO, NOT SINCE 1938!"
+    else 
       "HUH?! SPEAK UP, SONNY!"
   end
 end
